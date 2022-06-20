5ffcb7e57a62e4434197946ddd21b246aa119efd
tktktakatuska <blue.mountain.syota@iclouud.com>
2022-06-20 21:07:04 +0900

    任意のコメント

---
+++ Main/src/test/Factory.java
@@ -0,0 +1,13 @@
+package test;
+
+public abstract class Factory {
+
+    public final Product create(String owner) {
+        Product product = createProduct(owner);//※
+        registerProduct(product);
+        return product;
+    }
+
+    protected abstract Product createProduct(String owner);
+    protected abstract void registerProduct(Product product);
+}
